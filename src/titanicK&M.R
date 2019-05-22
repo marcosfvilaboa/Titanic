169f9ff8ea6a612bf9a197a31d0888836cb6c4fc
Marcos F. Vilaboa <marcosfvilaboa@gmail.com>
2019-05-22 19:56:58 +0200

    Init project

---
+++ src/titanicK&M.R
@@ -0,0 +1,7 @@
+##################
+####  Titanic  
+##################
+
+# ---1. Càrrega de dades---
+titanic.original <- read.csv("~/R/TCVD/Titanic/titanic_train.csv", header=TRUE)
+titanic <- titanic.original
